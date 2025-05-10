a03f119866f715dc2bf69f006956a01a06ee84c0
Guilherme domingos <e107ti@etec.sp.gov.br>
2025-05-09 21:47:43 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿double alt, ct1, ct2;
+
+string sx;
+
+Console.Clear();
+
+Console.WriteLine("--- Saiba seu peso ideal ---");
+
+Console.WriteLine("Digite sua altura em metros");
+alt = Convert.ToDouble(Console.ReadLine()); 
+
+Console.WriteLine("Sexo [M]asculino / [F]eminino...:");
+sx = Console.ReadLine();
+
+ct1 = alt * 72.7 - 58;
+
+ct2 = alt * 62.1 - 44.7;
+
+if ( sx == "m" )
+{
+    Console.WriteLine($"Seu peso ideal é {ct1}");
+}
+else if ( sx == "f" )
+{
+    Console.WriteLine($"Seu peso ideal é {ct2}");
+}
